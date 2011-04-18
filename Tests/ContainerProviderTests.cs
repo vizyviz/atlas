ad8fbdb453c763762adbdc48c656d5059c68047b
adam_miller <adam_miller@VHT4829LN1.vhtchicago.vht.com>
2011-04-18 11:29:29 -0500

    added ignores and missing test

---
+++ Tests/ContainerProviderTests.cs
@@ -0,0 +1,44 @@
+﻿using Autofac.Core.Lifetime;
+using Autofac.Core.Registration;
+using Autofac;
+using Atlas;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ContainerProviderTests
+    {
+        private IContainer _applicationContainer;
+        private ContainerProvider _provider;
+
+        [SetUp]
+        public void Setup()
+        {
+            _applicationContainer = MockRepository.GenerateStrictMock<IContainer>();
+            _provider = ContainerProvider.Instance;
+            _provider.ApplicationContainer = _applicationContainer;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _applicationContainer.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void CreateUnitOfWorkContainer()
+        {
+            ILifetimeScope unitOfWorkContainer = new LifetimeScope(new ComponentRegistry());
+
+            _applicationContainer.Expect(ac => ac.BeginLifetimeScope()).Return(unitOfWorkContainer);
+            var actual = _provider.CreateUnitOfWorkContainer();
+
+            Assert.AreNotSame(actual, _applicationContainer);
+
+
+        }
+
+    }
+}
